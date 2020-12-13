9e1f5fadc28b0ca43fac71b8d33f9a4d660424d6
rahulxyz <rahulguptaxyz15@gmail.com>
2020-12-13 21:24:42 +0530

    post fetch, set value

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
+const baseUrl = "http://localhost:3000/api/url";
+
 let urlInput = document.getElementById("url");
-let shortUrl = document.getElementsByClassName("short-url");
-let timestamp = document.getElementsByClassName("timestamp");
+let shortUrl = document.getElementById("short-url");
+let timestamp = document.getElementById("timestamp");
+let bigUrl = document.getElementById("big-url");
+let tooltip = document.getElementById("big-url-tooltip");
 
 let submitBtn = document.getElementById("submit-url");
 
-
 const endpoint = "https://restcountries.eu/rest/v2/all";
 
-url.addEventListener("keyup", (event)=>{
+url.addEventListener("keyup", (event) => {
     if (event.keyCode === 13) {
         event.preventDefault();
         submitUrl();
     }
 })
 
-const submitUrl = ()=>{
-    console.log(">>>>>>>>>> ", urlInput.value, shortUrl.item(0).innerHTML, timestamp.item(0).innerHTML);
+const submitUrl = () => {
+    const payload = {
+        url: urlInput.value
+    }
+    shortUrl.href="http://asda";
+    fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    }).then((res) => {
+        shortUrl.innerHTML = res.tinyUrl;
+        shortUrl.href = res.tinyUrl;
+        bigUrl.href =  res.bigUrl;
+        tooltip.innerHTML = res.bigUrl;
+    });
+
     urlInput.value = "";
-    fetch(endpoint).then((res)=>{
-        console.log(">>>>> ", res);
-    })
+    payload.url = "";
 }
+
+
+
